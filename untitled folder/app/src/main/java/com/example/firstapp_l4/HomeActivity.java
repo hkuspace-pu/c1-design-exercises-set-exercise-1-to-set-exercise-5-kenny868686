d3a83c4a6e1ef256e00b7a5bf0be2855ac69e17a
kenny868686 <20286942@learner.hkuspace.hku.hk>
2025-11-30 18:57:22 +0800

    Final

---
+++ untitled folder/app/src/main/java/com/example/firstapp_l4/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstapp_l4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button bookButton = findViewById(R.id.buttonBookTable);
+        bookButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, SelectDateActivity.class);
+            startActivity(intent);
+        });
+    }
+}
